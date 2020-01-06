97b4c758f5cd75f3f765b2d8c3c5fad7ee8b6ed1
cedric e goddard <8ceg6@comcast.net>
2020-01-06 00:52:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,12 +8,13 @@ class Song
   @@artists= []
   
       def initialize(name,artist,genre)
-          @@count +=1 
-          @@genres << genre 
-          @@artists << artist
           @name= name
           @artist= artist
           @genre= genre 
+          @@count +=1 
+          @@genres << genre 
+          @@artists << artist
+          
       end 
       
       def self.count
