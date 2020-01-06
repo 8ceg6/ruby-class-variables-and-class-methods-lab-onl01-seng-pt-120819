6309d680838c1ef24c45c9706f167a7b1bd453ca
cedric e goddard <8ceg6@comcast.net>
2020-01-06 00:57:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,8 @@ class Song
           @artist= artist
           @genre= genre 
           @@count +=1 
-          @@genres << genre 
-          @@artists << artist
+          @@genres << genres 
+          @@artists << artists
           
       end 
       
