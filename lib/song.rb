6f3cee973a68b90fdbb19aab1fd5ecc91bff85d2
cedric e goddard <8ceg6@comcast.net>
2020-01-06 00:27:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@genres = []
+  @@artists= []
+  
+      def initialize
+          @@count += 1 
+          @@genres << genre 
+          @@artists << artist 
+      end 
+      
+      def self.count
+        
+      end 
+      
+      def self.genres 
+        
+      end 
+      
+      def self.genre_count
+        
+      end 
+  
+end 
